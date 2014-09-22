828bcc2af31cf6c05c1b6cadab963d5a664172f0
Jeremiah Currier <jcurrier@zendesk.com>
2014-09-22 09:16:31 +0100

    cleaning up app.js

--- app.js
+++ app.js
@@ -9,26 +9,13 @@
     },
 
     doSomething: function() {
-      // this.switchTo('spinner');
 
-      var currentAccount = this.currentAccount();
-      var subdomain = currentAccount.subdomain();
-
-      var ticket = this.ticket();
-      // console.log(ticket.customField("custom_field_22634584"));
-
-      var y = ticket.customField("custom_field_22634584");
-      // console.log(y + "   < ---  this is y");
-      
-      var z = this.setting('id'); // id of custom drop down field used
-      // console.log(z);
-
-      var t = ticket.customField("custom_field_" + z); // custom field from settings
-      // console.log(t + "   < ---  this is t");
-
-      // console.log(currentAccount.subdomain());
-
-      var a = 'api', // https://rfc.zendesk.com/hc/en-us/articles/203251130-API-API-API-ALL-DAY
+      var currentAccount = this.currentAccount(),
+          subdomain = currentAccount.subdomain(),
+          ticket = this.ticket(),
+          z = this.setting('id'), // id of custom drop down field used
+          t = ticket.customField("custom_field_" + z), // custom field from settings
+          a = 'api', // https://rfc.zendesk.com/hc/en-us/articles/203251130-API-API-API-ALL-DAY
           b = 'apps', // https://rfc.zendesk.com/hc/en-us/articles/203016354-APPS-APPS-APPS-ALL-DAY
           c = 'sso'; // https://rfc.zendesk.com/hc/en-us/articles/203016364-SSO-SSO-SSO-ALL-DAY
 
